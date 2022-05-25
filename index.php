e0e86342a92ac64e6de75d4e6c25d12fdcbc32d9
Eric Iwayan <79345596+Icon061997@users.noreply.github.com>
2022-05-25 20:39:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,25 @@
 <html lang="en">
 
 <head>
-    <title> Login Form</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <!-- sweet allert -->
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>    
-    <!-- ccss file -->
-    <link href="style.css" rel="stylesheet">
-    <!-- fa fa icon -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        hr {
+            border: 2px solid black;
+            width: 70%;
+            margin: auto;
+            border-bottom: none;
+            border-right: none;
+            border-left: none;
 
-
-
+        }
+    </style>
 
 </head>
 
@@ -25,41 +28,35 @@
 
     <div class="container-fluid">
         <div class="container_box">
+            <form action="main.php">
 
-        <form action="main.php" method="">
-            <div class="content-box">
                 <div class="container">
-                <img src="image/logo.jpg" alt="Flowers in Chania" class="img-box">
-                <p style="margin-top: 10px;"><b><i>simplePos</i></b>Admin</p>
+                    <img src="images/logo.png" alt="" class="img-box">
+                </div>
+                <h4 style="margin-top: 10px;"><b style="color: #1f3040;
+                    ;">Ez</b><b style="color: #1f3040;">Shop </b></b>Admin
+                </h4>
                 <hr>
-                </div>
 
-        
-                <div class="content">
-                <label for="username" class="">Username or email</label>
-                <input type="text" name="username" id="username" class="input-box" autocomplete="off" placeholder="  ">
-                </div>
 
-                <div class="content">
-                <label for="username">Password</label>
-                <input type="password" name="password" id="password" class="input-box" autocomplete="off" placeholder="  ">
-                </div>
-                <br>
-                    
-                <input type="submit" class="buttons" id="submit" name="submit" value="Login">
 
-             
-            </div>
+                <div class="input-fields">
+                    <input type="text" name="" id="" placeholder="" >
+                    <label for="">Username or Email</label>
+                </div>
+                <div class="input-fields">
+                    <input type="password" name="" id="" placeholder="" >
+                    <label for="">Password</label>
+
+                </div>
+                <input type="submit" value="Login" name="" id="btn">
+
             </form>
-
         </div>
     </div>
-
-
-
-
-
-
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+    crossorigin="anonymous"></script>
 
 </html>
